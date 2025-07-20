710e89547800628fe291126652166edd6869cf2d
ItsAviel <avielcohen259@gmail.com>
2025-07-20 20:22:53 +0300

    interface + implemention LRU

--- src/main/java/org/example/cache/CacheAlgo.java
+++ src/main/java/org/example/cache/CacheAlgo.java
@@ -1,4 +1,9 @@
 package org.example.cache;
 
 
-
+public interface CacheAlgo<K, V> {
+    void put(K key, V value);
+    V get(K key);
+    boolean containsKey(K key);
+    int size();
+}
--- src/main/java/org/example/cache/LRUCacheAlgo.java
+++ src/main/java/org/example/cache/LRUCacheAlgo.java
@@ -1,2 +1,38 @@
 package org.example.cache;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class LRUCacheAlgo<K, V> implements CacheAlgo<K, V> {
+    private final int capacity;
+    private final Map<K, V> cacheMap;
+
+    public LRUCacheAlgo(int capacity) {
+        this.capacity = capacity;
+        this.cacheMap = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > LRUCacheAlgo.this.capacity;
+            }
+        };
+    }
+
+    @Override
+    public void put(K key, V value) {
+        cacheMap.put(key, value);
+    }
+
+    @Override
+    public V get(K key) {
+        return cacheMap.get(key);
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return cacheMap.containsKey(key);
+    }
+
+    @Override
+    public int size() {
+        return cacheMap.size();
+    }
+}
